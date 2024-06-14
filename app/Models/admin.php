9b8df2a68368a148c62cd57ec91d0f86210cdde4
Amanda Bee <145218592+AmandaBee04@users.noreply.github.com>
2024-06-15 05:19:08 +0800

    Add files via upload

---
+++ app/Models/admin.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class admin extends Model
+{
+    protected $fillable =[
+        'id', 
+        'name',
+        'password',
+        'profile_picture',
+        'email'
+    ];
+
+    protected $hidden = [
+        'password'
+    ];
+
+    public $timestamps = false;
+    use HasFactory;
+}
